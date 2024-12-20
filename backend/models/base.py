171597bd716741808e20b90e76acdacc492e7029
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-20 17:06:05 +0100

    feat(database): add base class as abstract

---
+++ backend/models/base.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Contains class Base
+"""
+from __init__ import db
+from datetime import datetime, timezone
+
+
+class Base(db.Model):
+    """ The Abstract Base class,
+    from which future classes will be derived"""
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        server_default=db.func.now(), nullable=False)
+    # created_at = db.Column(db.DateTime(timezone=True),
+    # nullable=False, default=datetime.utcnow)
+
+    # def new(self, obj):
+    #     """ add new object to database"""
+    #     db.session.add(obj)
+
+    # def save(self):
+    #     """ save object to database """
+    #     db.session.commit
+
+    # def delete(self, obj=None):
+    #     if obj is not None:
+    #         db.session.delete(obj)
+    
+    # def all(self, cls=None):
+    #     """ query on the current database session """
+    #     if cls:
+    #         return db.session.query(cls).all()
+
+    #also find, update
