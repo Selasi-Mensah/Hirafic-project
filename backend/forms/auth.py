91bd1a72bea8ddc93ace9288619d69f8057f2f09
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-20 17:56:38 +0100

    feat(auth): - add Registration form - add Login form

---
+++ backend/forms/auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+"""
+Contains Registration and Login Forms
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, RadioField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models.user import User
+
+
+class RegistrationForm(FlaskForm):
+    """ This form for registration """
+    username = StringField('Username', validators=[
+                                DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                                        DataRequired(),
+                                                        EqualTo('password')])
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+    role = RadioField(
+        'Role',
+        choices=[('Artisan', 'Artisan'), ('Client', 'Client')],
+        validators=[DataRequired()])
+
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        """ to validate username """
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken!')
+
+    def validate_email(self, email):
+        """ to validate password """
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already taken!')
+
+class LoginForm(FlaskForm):
+    """ This form for Login """
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                                            DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
