e1701a2205956c44949181bb52cf17b5e09d2a2f
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-21 19:32:02 +0100

    feat(structure): commit init files, run file and requirements

---
+++ backend/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+# creating flask instant and db
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'e4307d4b50f2d467b26d69469749871a'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from models.user import User
+from models.client import Client
+from models.artisan import Artisan
+from models.booking import Booking
+from routes import auth
+
+with app.app_context():
+    db.create_all()
