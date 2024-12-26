152f698df2f259313fde97473799bd738275d4b1
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-26 18:03:15 +0100

    feat(API): add ability to update profile picture and save it with small size in static

--- backend/routes/artisan.py
+++ backend/routes/artisan.py
@@ -2,6 +2,9 @@
 """
 API for artisan
 """
+import os
+import secrets
+from PIL import Image
 from flask import Blueprint
 from __init__ import db, bcrypt
 from models.user import User
@@ -15,11 +18,30 @@ from flask_login import login_user, current_user, logout_user, login_required
 artisans_Bp = Blueprint('artisans', __name__)
 
 
+
+def save_picture(form_picture):
+    """ function to save the updated profile picture"""
+    random_hex = secrets.token_hex(8)
+    _, file_ext = os.path.splitext(form_picture.filename)
+    pic_fname = random_hex + file_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', pic_fname)
+    
+    output_size = (125, 125)
+    open_image = Image.open(form_picture)
+    open_image.thumbnail(output_size)
+    
+    open_image.save(picture_path)
+    return pic_fname
+
+
 @artisans_Bp.route("/artisan", methods=['GET', 'POST'])
 @login_required
 def artisan_profile():
     form = ArtisanProfileForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         current_user.phone_number = form.phone_number.data
